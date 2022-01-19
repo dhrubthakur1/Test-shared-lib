54d5f375edbbab4f7da4425421444d6b7ca25bcd
dhrubthakur1 <dhrubthakur@gmail.com>
2022-01-19 09:57:25 +0530

    Update TestSrc.groovy

--- src/com/TestSrc.groovy
+++ src/com/TestSrc.groovy
@@ -5,6 +5,8 @@ public TestSrc(){
 println("In contructor....");
 }
 public String displayName(String name){
+  println("DisplayName... ${name}")
+  System.out.println("DisplayName... ${name}")
   return "From TestSrc ${name}"
 }
 }
